e5c11096f93c690cb79e9e2d7df5c148134ffea3
Tanyoung Kim <tanyoung.kim@airbnb.com>
2021-12-28 15:11:52 -0800

    Updates

---
+++ precache-manifest.fdda25dff4b4de6e594d3c37096d58a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b335d4da47d53652698",
+    "url": "/knitting/static/css/main.82b5af20.chunk.css"
+  },
+  {
+    "revision": "7b335d4da47d53652698",
+    "url": "/knitting/static/js/main.7b335d4d.chunk.js"
+  },
+  {
+    "revision": "dbf16a7a1f5d12f53526",
+    "url": "/knitting/static/js/1.dbf16a7a.chunk.js"
+  },
+  {
+    "revision": "b75323fd636df612d81b",
+    "url": "/knitting/static/js/2.b75323fd.chunk.js"
+  },
+  {
+    "revision": "b9b6da85df3115b08cd0",
+    "url": "/knitting/static/js/runtime~main.b9b6da85.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/knitting/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6daa65b75e401510d1046cf77c0be8e2",
+    "url": "/knitting/index.html"
+  }
+];
